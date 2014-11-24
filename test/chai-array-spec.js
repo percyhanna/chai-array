a2d98792206773c2b0d7e85a50bc05087966aa7f
Eric Kryski <e.kryski@gmail.com>
2014-11-24 13:04:24 -0700

    adding a few more array helpers

--- test/chai-array-spec.js
+++ test/chai-array-spec.js
@@ -47,6 +47,46 @@ describe("React assertions", function() {
     });
   });
 
+  describe('second', function () {
+    it('changes the object to the second item in the array', function () {
+      expect([1, 2, 3]).second.to.equal(2);
+    });
+
+    it('returns second character for string values', function () {
+      expect('abc').second.to.equal('b');
+    });
+
+    it('returns undefined for numeric values', function () {
+      expect(1).second.to.equal(undefined);
+    });
+
+    it('fails for null values', function () {
+      expect(function () {
+        expect(null).second.to.equal(null);
+      }).to.fail('expected null to exist');
+    });
+  });
+
+  describe('third', function () {
+    it('changes the object to the third item in the array', function () {
+      expect([1, 2, 3]).third.to.equal(3);
+    });
+
+    it('returns third character for string values', function () {
+      expect('abc').third.to.equal('c');
+    });
+
+    it('returns undefined for numeric values', function () {
+      expect(1).third.to.equal(undefined);
+    });
+
+    it('fails for null values', function () {
+      expect(function () {
+        expect(null).third.to.equal(null);
+      }).to.fail('expected null to exist');
+    });
+  });
+
   describe('last', function () {
     it('changes the object to the last item in the array', function () {
       expect([1, 2, 3]).last.to.equal(3);
